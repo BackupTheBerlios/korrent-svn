be7d111e3a22c0805ba25fba477dc634c6c9028c
rst <rst@bfcf333f-f6e3-0310-8eda-84c3f4ff2649>
2004-10-16 14:39:58 +0000

    Some not so important about data

--- korrent/main.cpp
+++ korrent/main.cpp
@@ -5,13 +5,13 @@
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -33,7 +33,7 @@
 #include <klocale.h>
 
 static const char description[] =
-    I18N_NOOP("A KDE Application");
+    I18N_NOOP("Korrent is a file transfer utility that uses the BitTorrent protocol.");
 
 static const char version[] = "v0.1";
 
@@ -45,8 +45,14 @@ static KCmdLineOptions options[] =
 
 int main(int argc, char **argv)
 {
-    KAboutData about("korrent", I18N_NOOP("Korrent"), version, description, KAboutData::License_GPL, "(C) 2004 Roberto Teixeira", 0, 0, "roberto@kde.org");
-    about.addAuthor( "Roberto Teixeira", 0, "roberto@kde.org" );
+    KAboutData about("korrent",
+                     I18N_NOOP("Korrent"),
+                     version, description,
+                     KAboutData::License_BSD,
+                     "(C) 2004 Roberto Teixeira",
+                     0, 0, "roberto@kde.org");
+    about.addAuthor( "Roberto Teixeira", i18n( "Lead developer/maintainer" ), "roberto@kde.org" );
+    about.addCredit( "Bram Cohen", i18n( "BitTorrent creator" ), 0, "http://bitconjurer.org/" );
     KCmdLineArgs::init(argc, argv, &about);
     KCmdLineArgs::addCmdLineOptions(options);
     KApplication app;
